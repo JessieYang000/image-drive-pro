6278cdaa9660a4212b191ed495974efc47c10513
JessieYang000 <yang.jieq@northeastern.edu>
2024-02-01 10:12:20 -0500

    MySQL database setup

--- backend/server.js
+++ backend/server.js
@@ -17,3 +17,22 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+
+// Create a connection to the MySQL database
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'test$$$123',
+    database: 'divisin',
+  });
+  
+  // Connect to the database
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+      return;
+    }
+    console.log('Connected to MySQL database');
+    connection.release(); // Release the connection immediately
+  });
+  
