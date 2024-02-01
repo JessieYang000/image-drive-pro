6ae6107f3ba36143631f5a18899b5e813ecd763d
JessieYang000 <yang.jieq@northeastern.edu>
2024-02-01 11:10:35 -0500

    add conditions to show or the logout button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
-import Login from './components/signin';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthProvider, useAuth } from './components/AuthContext';
+import Signin from './components/signin';
 import SignUp from './components/signup';
-import ImageStorage from './components/images';
+import Images from './components/images';
 
-function App() {
+function AppWrapper() {
+  const location = useLocation();
   const navigate = useNavigate();
-
+  const isAuthRoute = location.pathname === '/sign-in' || location.pathname === '/sign-up' || location.pathname === '/';
   const handleLogout = () => {
     navigate('/sign-in');
   }
-  return (
-    <Router>
-      <div className={'auth-wrapper'}>
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={'/sign-in'}>
-              Image Drive Pro
-            </Link>
-          </div>
 
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <div className={`App ${isAuthRoute ? 'auth-wrapper' : ''}`}>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={'/sign-in'}>
+            Image Drive Pro
+          </Link>
+        </div>
+        {isAuthenticated && !isAuthRoute && (
           <div style={{ marginRight: "20px" }}>
             <button
               style={{
@@ -36,18 +40,27 @@ function App() {
             >
               Logout
             </button>
-          </div>
-        </nav>
+          </div>)}
+      </nav>
 
-        <div className={'auth-inner'}>
-          <Routes>
-            <Route exact path="/" element={<Login />} />
-            <Route path="/sign-in" element={<Login />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/images" element={<ImageStorage />} />
-          </Routes>
-        </div>
+      <div className={isAuthRoute ? 'auth-inner' : ''}>
+        <Routes>
+          <Route exact path="/" element={<Signin />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/images" element={<Images />} />
+        </Routes>
       </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <AppWrapper />
+      </AuthProvider>
     </Router>
   );
 }
