7de443e6e4cbe8c3465aa449e6aff579179a48b1
JessieYang000 <yang.jieq@northeastern.edu>
2024-02-01 10:51:53 -0500

    add signin/signup/images components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Signin from './components/signin';
+import SignUp from './components/signup';
+import Images from './components/images';
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route path="/sign-in" element={<Login />} />
+          <Route exact path="/" element={<Signin />} />
+          <Route path="/sign-in" element={<Signin />} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/images" element={<ImageStorage />} />
+          <Route path="/images" element={<Images />} />
         </Routes>
       </div>
     </Router>
